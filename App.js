7b36a70732207d95aa5d76e8154561bc35944b98
Esmeralda Ochoa Flores <ing.esmeralda.of@gmail.com>
2023-01-16 17:33:57 -0600

    App.js cleaned.
    Co-authored-by: Camila Pinzón <Camilapinz18@users.noreply.github.com>

--- App.js
+++ App.js
@@ -6,7 +6,7 @@ import {
 } from 'react-native'
 import { Formik } from 'formik'
 import Checkbox from 'expo-checkbox'
-import { CustomInput} from './components/CustomInput'
+import { CustomInput } from './components/CustomInput'
 import { controls, containers, texts } from './styles/AppStyle.js'
 import CustomButton from './components/CustomButton'
 import CustomUnderlined from './components/CustomUnderlined'
@@ -22,7 +22,6 @@ export default function App() {
   const [isChecked2, setChecked2] = useState(false)
   const [inputText, setInputText] = useState('')
 
-
   return (
     <View style={containers.container}>
       <ScrollView>
@@ -35,6 +34,7 @@ export default function App() {
         >
           {({ handleChange, values, errors }) => (
             <View style={containers.screenContainer}>
+
               <Text style={texts.titlesText}>First Name</Text>
               <CustomInput
                 handleChange={handleChange}
@@ -44,30 +44,25 @@ export default function App() {
               />
 
               <Text style={texts.titlesText}>Email *</Text>
-
               <CustomInput
                 handleChange={handleChange}
                 values={values}
                 type='text'
                 onChangeText={value => setInputText(value)}
-
               />
-              <Text style={texts.titlesText} type='text'>
-                Password *
-              </Text>
 
+              <Text style={texts.titlesText} type='text'>Password *</Text>
               <CustomInput
                 handleChange={handleChange}
                 values={values}
                 type='password'
                 onChangeText={value => setInputText(value)}
               />
-
               {errors.password ? <Text>{errors.password}</Text> : <></>}
               <Text style={texts.warningPassword}>
-                Use 8 or more characters with a mix of letters, numbers and
-                symbols
+                Use 8 or more characters with a mix of letters, numbers and symbols
               </Text>
+
               <View style={containers.checkContainer}>
                 <Checkbox
                   value={isChecked}
@@ -78,11 +73,12 @@ export default function App() {
                 <View style={containers.underlinedTextContainer}>
                   <Text style={texts.textCheck}>I agree to the </Text>
                   <CustomUnderlined text='Terms' color='gray' />
-                  <Text style={texts.textCheck}> and </Text> 
-                  <CustomUnderlined text=' Privacy Policy.' color='gray'/>
-                  <Text style={texts.asterisk}> *</Text> 
+                  <Text style={texts.textCheck}> and </Text>
+                  <CustomUnderlined text=' Privacy Policy.' color='gray' />
+                  <Text style={texts.asterisk}> *</Text>
                 </View>
               </View>
+
               <View style={containers.checkContainer}>
                 <Checkbox
                   value={isChecked2}
@@ -94,28 +90,28 @@ export default function App() {
                   Suscribe for select product updates
                 </Text>
               </View>
+
               <View style={containers.buttonsContainer}>
                 {isChecked && isChecked2 === true ?
-                  <CustomButton text='Sign Up' disabled={false} icon={false}/> :
+                  <CustomButton text='Sign Up' disabled={false} icon={false} /> :
                   <CustomButton text='Sign Up' disabled={true} icon={false} />
                 }
-
                 <Text style={texts.accountText}>or</Text>
-
                 {isChecked && isChecked2 === true ?
-                  <CustomButton text='Sign Up with Google' disabled={false} icon={true}/> :
-                  <CustomButton text='Sign Up with Google' disabled={true} icon={true}/>
+                  <CustomButton text='Sign Up with Google' disabled={false} icon={true} /> :
+                  <CustomButton text='Sign Up with Google' disabled={true} icon={true} />
                 }
 
                 <View style={containers.footerContainer}>
                   <Text style={texts.accountText}>
                     Already have an account?{' '}
                   </Text>
-                  <CustomUnderlined text='Login' 
+                  <CustomUnderlined text='Login'
                     color='purple'
                   />
                 </View>
               </View>
+
             </View>
           )}
         </Formik>
